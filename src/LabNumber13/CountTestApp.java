4437fab2482773dc6e249564830d9483edb7a106
yas <yaslee3@yahoo.com>
2016-11-03 16:25:52 -0400

    Initial commit

---
+++ src/LabNumber13/CountTestApp.java
@@ -0,0 +1,13 @@
+package LabNumber13;
+
+public class CountTestApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Counting alligators...");
+
+
+	}
+
+}
